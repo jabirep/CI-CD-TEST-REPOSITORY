ab29336aa8f3a971d102897fcba24567affe1537
Jabir <149050045+jabirep@users.noreply.github.com>
2025-03-12 14:35:29 +0530

    Update AppTest.java

--- CI-CDTEST/src/test/java/CI_CDTEST/AppTest.java
+++ CI-CDTEST/src/test/java/CI_CDTEST/AppTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.By;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
